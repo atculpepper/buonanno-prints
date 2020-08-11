8866c926b28453137b119a7a0dd79c4ed2afa866
Anne Culpepper <anne.culpepper@gmail.com>
2020-08-11 10:43:23 -0500

    deleted extraneous dev dependencies and deleted a node modules folder higher up in tree. Getting proxy errors between client and server

--- server/routes/images.router.js
+++ server/routes/images.router.js
@@ -1,11 +1,5 @@
 const express = require('express');
 const pool = require('../modules/pool');
-//dependencies from imran sayed tutorial
-const aws = require('aws-sdk');
-const multerS3 = require('multer-s3');
-const multer = require('multer');
-const path = require('path');
-const url = require('url');
 
 const router = express.Router();
 
